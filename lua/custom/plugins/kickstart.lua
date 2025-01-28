3be658bb3bf95d6b2c62ebfde1c2e617f558e8d2
Nicolas Rannou <nicolas.rannou@spinergie.com>
2025-01-28 20:57:35 +0100

    update config

--- lua/custom/plugins/kickstart.lua
+++
@@ -1,8 +0,0 @@
-return {
-  require("kickstart.plugins.debug"),
-  require("kickstart.plugins.lint"),
-  require("kickstart.plugins.gitsigns"),
-  require("kickstart.plugins.neo-tree"),
-  require("kickstart.plugins.autopairs"),
-  require("kickstart.plugins.indent_line"),
-}
