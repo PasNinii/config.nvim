0f72f079011912cb7f04dde851a5178d453515bf
Nicolas Rannou <nicolas.rannou@spinergie.com>
2025-06-16 14:23:47 +0200

    init

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    javascript = { "dprint", "prettierd", "prettier", stop_after_first = true },
+    typescript = { "dprint", "prettierd", "prettier", stop_after_first = true },
+    html = { "dprint", "prettierd", "prettier", stop_after_first = true },
+    css = { "dprint", "prettierd", "prettier", stop_after_first = true },
+    scss = { "dprint", "prettierd", "prettier", stop_after_first = true },
+  },
+}
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,14 @@
+return {
+  { "folke/flash.nvim", enabled = false },
+
+  {
+    "folke/snacks.nvim",
+    opts = {
+      notifier = { enabled = false },
+      notify = { enabled = false },
+      scroll = { enabled = false },
+      animate = { enabled = false },
+      terminal = { enabled = false },
+    },
+  },
+}
