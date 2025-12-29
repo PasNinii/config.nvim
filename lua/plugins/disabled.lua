60732bf23ed606e62cef63a1df5d15a6bfb547d6
Nicolas Rannou <nicolas.rannou@spinergie.com>
2025-12-29 12:18:29 +0100

    fix

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,11 +5,12 @@ return {
     "folke/snacks.nvim",
     opts = {
       notifier = { enabled = false },
-      notify = { enabled = false },
-      scroll = { enabled = false },
-      animate = { enabled = false },
-      terminal = { enabled = false },
       explorer = { enabled = false },
+      -- notify = { enabled = false },
+      -- scroll = { enabled = false },
+      -- animate = { enabled = false },
+      -- terminal = { enabled = false },
+      -- picker = { enabled = false },
     },
   },
 }
