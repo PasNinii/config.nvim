18c2cf5e9840187d97ecec6b97ad1b23108418c8
PasNinii <nicrannou@gmail.com>
2026-06-22 12:19:54 +0200

    shortcut

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -7,9 +7,16 @@ return {
         discussion = { add_comment = { lhs = "<localleader>cc", desc = "add comment" } },
         issue = { add_comment = { lhs = "<localleader>cc", desc = "add comment" } },
         pull_request = { add_comment = { lhs = "<localleader>cc", desc = "add comment" } },
-        review_thread = { add_comment = { lhs = "<localleader>cc", desc = "add comment" } },
         review_diff = {
           add_review_comment = { lhs = "<localleader>cc", desc = "add review comment", mode = { "n", "x" } },
+          -- drive file nav from the diff window without returning to the file panel
+          select_next_entry = { lhs = "<C-n>", desc = "next changed file" },
+          select_prev_entry = { lhs = "<C-p>", desc = "previous changed file" },
+        },
+        review_thread = {
+          add_comment = { lhs = "<localleader>cc", desc = "add comment" },
+          select_next_entry = { lhs = "<C-n>", desc = "next changed file" },
+          select_prev_entry = { lhs = "<C-p>", desc = "previous changed file" },
         },
       },
     },
