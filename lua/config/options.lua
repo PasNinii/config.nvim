0d1d5025968a2f0528015f5348126cc3c1b5ab6b
PasNinii <nicrannou@gmail.com>
2026-06-22 12:16:40 +0200

    options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Snacks animations
 -- Set to `false` to globally disable all snacks animations
 vim.g.snacks_animate = false
+
+-- octo.nvim's keymaps are all <localleader>-based; align localleader with leader (space)
+-- so they fire on space (e.g. <localleader><space> = space-space to toggle viewed)
+vim.g.maplocalleader = " "
